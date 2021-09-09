9fa937dc6d8add5483abcaa38cef2913a2339afc
shravya p <48832081+shraazp@users.noreply.github.com>
2021-09-09 10:51:19 +0530

    Refactored: player is initialised

--- Deck.java
+++ Deck.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Deck {
 	private List<Card> cards;
 	/**
-    * Constructs a standard deck of 52 cards.
-    */
-	private static final String[] RANKS = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    	* Constructs a standard deck of 52 cards.
+   	 */
+	private static final List<String> RANKS = new ArrayList<String>(List.of("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"));
 	private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
 	public Deck() {
         this.cards = new ArrayList<Card>(52);
